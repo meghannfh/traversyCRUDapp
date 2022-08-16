d09ea3a6e39019624e7c80bb7e3cdbe5fea79dd6
meghann <meghannfh@gmail.com>
2022-08-15 18:20:09 -0600

    working on dashboard styling

--- app.js
+++ app.js
@@ -1,53 +1,87 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+//!Change: Mongoose is no longer required
+//const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
-const connectDB = require('./config/db')
+const methodOverride = require('method-override')
 const passport = require('passport')
+const session = require('express-session')
+//!Change: MongoStore does not requre (session)
+const MongoStore = require('connect-mongo')//(session)
+const connectDB = require('./config/db')
 
-// load config file
-dotenv.config({ path: './config/config.env'})
+// LOAD CONFIG
+dotenv.config({path: './config/config.env'})
 
-//Passport config
+//LOAD PASSPORT
 require('./config/passport')(passport)
 
 connectDB()
 
 const app = express()
 
-//Logging
-if(process.env.NODE_ENV === 'development'){
+//BODY PARSER
+app.use(express.urlencoded({ extended: false}))
+app.use(express.json())
+
+//DETERMINE LEVEL OF LOGGING
+if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+//HELPERS
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
+
 //Handlebars
-//!Add the word .engine after exphbs
-app.engine('.hbs', exphbs.engine({
-    defaultLayout: 'main',
-    extname: '.hbs'
+app.engine(
+    '.hbs', 
+    //!Change: add '.engine' after exphbs
+    exphbs.engine({
+        helpers: {
+            formatDate,
+            stripTags,
+            truncate,
+            editIcon,
+            select
+        },
+        defaultLayout: 'main',
+        extname: '.hbs'
     })
-)
-app.set('view engine', '.hbs')
+);
+app.set('view engine', '.hbs');
 
-//Sessions
-app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}))
+// SESSIONS
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      //!Change: MongoStore syntax has changed
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+      })
+    })
+  )
 
-//Passport middleware
+// PASSPORT MIDDLEWARE
 app.use(passport.initialize())
 app.use(passport.session())
 
-//Static folder
+//SET GLOBAL VARIABLE
+app.use(function(req, res, next){
+    res.locals.user = req.user || null
+    next()
+})
+
+//STATIC FOLDER
 app.use(express.static(path.join(__dirname, 'public')))
 
-//Routes
+//ROUTES
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 8500
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
+const Story = require('../models/Story')
 
 // @desc Login/Langing page
 // @route GET /
@@ -12,9 +13,17 @@ router.get('/', ensureGuest, (req, res) => {
 
 //@desc Dashboard
 //@Route GET / dashboard
-router.get('/dashboard', ensureAuth, (req, res) => {
-    console.log(req.user)
-    res.render('dashboard')
+router.get('/dashboard', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ user: req.user.id }).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
 })
 
 
